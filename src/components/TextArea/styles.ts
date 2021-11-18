bab50b5e5d6fda8f841f6244344d217d1f6cd5a0
NatanTavares <nattantavares.s15@gmail.com>
2021-11-17 21:06:27 -0300

    feat(styles)

--- src/components/TextArea/styles.ts
+++ src/components/TextArea/styles.ts
@@ -4,4 +4,25 @@ type ContainerProps = {
   error: boolean;
 };
 
-export const Container = styled.label<ContainerProps>``;
+export const Container = styled.label<ContainerProps>`
+  p {
+    font: ${({ theme }) => theme.font.navigationLabel};
+    font-size: 1rem;
+    margin-bottom: ${({ theme }) => theme.scales.spacing.md};
+  }
+
+  textarea {
+    resize: none;
+
+    height: 100vh;
+    max-height: 200px;
+    width: 100%;
+
+    padding: ${({ theme }) => theme.scales.spacing.sm};
+
+    border: 1px solid;
+    border-radius: ${({ theme }) => theme.borderRadius.sm};
+    border-color: ${({ theme, error }) =>
+      error ? theme.palette.red : theme.palette.gray};
+  }
+`;
